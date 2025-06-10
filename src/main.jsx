8ee090a5aa83aa019891917c18190381a36b8cdb
wooseok kang <135770763+usewooseok97@users.noreply.github.com>
2025-06-11 02:13:48 +0900

    베포버전0.9

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router'
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={import.meta.env.DEV ? '/Aclass_team_1' : '/'}>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
     <App />
     </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Aclass_team_1/' ,
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    base: env.VITE_BASE_URL ?? '/',
+    plugins: [react()],
+  })
+}
