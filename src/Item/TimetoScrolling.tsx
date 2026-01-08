a87c82681f4011b9fa5073b0ce1dd04000577f35
wooseok kang <135770763+usewooseok97@users.noreply.github.com>
2026-01-08 09:52:23 +0900

    전체 구조 body -body연결되는 에러 수정 , pictures 수정

---
+++ src/Item/TimetoScrolling.tsx
@@ -0,0 +1,13 @@
+const TimetoScrolling = () => {
+  return (
+    <article>
+      <div>
+        <div className="border">
+          <div className="border rounded-b-full w-6 h-6 "></div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default TimetoScrolling;
