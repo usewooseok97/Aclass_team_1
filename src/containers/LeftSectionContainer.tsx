63a7968b06e977775d4e23ac7dbaa3496d3d6cab
wooseok kang <135770763+usewooseok97@users.noreply.github.com>
2026-01-23 10:28:14 +0900

    backbutton 레이아웃 수정 완료

--- src/containers/LeftSectionContainer.tsx
+++ src/containers/LeftSectionContainer.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 const LeftSectionContainer = ({ children, className = "" }: ContainerProps) => {
   return (
     <section
-      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col items-center border border-black/50 rounded-tr-[40px] rounded-br-[40px] ${className}`}
+      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col justify-between items-center rounded-tr-[40px] rounded-br-[40px] relative ${className} `}
     >
       {children}
     </section>
--- src/containers/RightSectionContainer.tsx
+++ src/containers/RightSectionContainer.tsx
@@ -8,7 +8,10 @@ interface ContainerProps {
 const RightSectionContainer = ({ children, className = "" }: ContainerProps) => {
   return (
     <section
-      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col items-center border border-black/50 rounded-tl-[40px] rounded-bl-[40px] ${className}`}
+      style={{
+        borderColor: 'var(--card-border)',
+      }}
+      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col items-center border rounded-tl-[40px] rounded-bl-[40px] relative ${className}`}
     >
       {children}
     </section>
