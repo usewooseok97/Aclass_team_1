2337c136d6a732f732a16f8f4d912b19b1b3407b
wooseok kang <135770763+usewooseok97@users.noreply.github.com>
2026-01-24 11:11:03 +0900

    상세페이지 레이아웃 수정 + 스크롤 해결

---
+++ src/containers/LeftContentDetail.tsx
@@ -0,0 +1,38 @@
+import { useFestivalContext } from "@hooks/useFestivalContext";
+import { CardLayout } from "@atoms/CardLayout";
+import { FestivalDetailInfo } from "@pages/FestivalDetailPage/FestivalDetailInfo";
+import { FestivalDescription } from "@pages/FestivalDetailPage/FestivalDescription";
+import { FestivalActionButtons } from "@pages/FestivalDetailPage/FestivalActionButtons";
+import { NearbyRestaurants } from "@/pages/FestivalDetailPage/NearbyRestaurants";
+
+const LeftContentDetail = () => {
+  const { selectedFestival , nearbyPlaces } = useFestivalContext();
+
+  if (!selectedFestival) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-4 w-full h-full p-4 overflow-y-auto scrollbar-hide">
+      {/* 상세 정보 */}
+      <CardLayout>
+        <div className="p-4 w-full">
+          <FestivalDetailInfo festival={selectedFestival} />
+        </div>
+        <div className="p-4 w-full">
+          <FestivalDescription program={selectedFestival.PROGRAM} />
+          <FestivalActionButtons homepageUrl={selectedFestival.HMPG_ADDR} />
+        </div>
+      </CardLayout>
+
+      {/* 축제 소개 + 버튼 */}
+      <CardLayout>
+        <div className="p-4 w-full">
+          <NearbyRestaurants places={nearbyPlaces} />
+        </div>
+      </CardLayout>
+    </div>
+  );
+};
+
+export default LeftContentDetail;
--- src/containers/RightSectionContainer.tsx
+++ src/containers/RightSectionContainer.tsx
@@ -11,7 +11,7 @@ const RightSectionContainer = ({ children, className = "" }: ContainerProps) => 
       style={{
         borderColor: 'var(--card-border)',
       }}
-      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col items-center border rounded-tl-[40px] rounded-bl-[40px] relative ${className}`}
+      className={`h-[calc(100vh-200px)] min-h-206 w-[40%] sm:w-[60%] lg:w-[40%] 2xl:max-w-200 min-w-146 flex flex-col items-center border rounded-tl-[40px] rounded-bl-[40px] relative ${className} p-12 scrollbar-hide`}
     >
       {children}
     </section>
